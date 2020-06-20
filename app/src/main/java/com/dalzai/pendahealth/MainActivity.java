ba810cc70728d851e10fc6f633aa5a9a195099e8
JackMutobu <jmutobu@outlook.com>
2020-06-20 10:33:24 +0300

    Added profile tab

--- app/src/main/java/com/dalzai/pendahealth/MainActivity.java
+++ app/src/main/java/com/dalzai/pendahealth/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.dalzai.pendahealthv1.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
